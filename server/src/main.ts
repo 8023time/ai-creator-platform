51325cd8fa2afd1f09654fd10a014b9f50a138ad
8023time <2162105974@qq.com>
2026-05-25 02:23:29 +0800

    feat: 添加 Swagger 自动生成接口文档

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@libs/common/filter/http-exception.filter';
 import { HttpResponseInterceptor } from '@libs/common/interceptor/http-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalInterceptors(new HttpResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('AI Creator Platform API')
+    .setDescription('AI 创作者平台后端接口文档')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
