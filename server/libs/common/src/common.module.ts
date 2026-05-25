e48666987f250261068e143328834c2358fadf40
8023time <2162105974@qq.com>
2026-05-25 17:48:07 +0800

    feat: 添加jwt实现双token实现身份识别

--- server/libs/common/src/common.module.ts
+++ server/libs/common/src/common.module.ts
@@ -2,11 +2,31 @@ import { Module, Global } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResponseModule } from './response/response.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
   providers: [CommonService],
-  exports: [CommonService, PrismaModule, ResponseModule],
-  imports: [PrismaModule, ResponseModule],
+  exports: [CommonService, PrismaModule, ResponseModule, JwtModule, ConfigModule],
+  imports: [
+    PrismaModule,
+    ResponseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get<string>('SECRET_KEY'),
+          signOptions: { expiresIn: '7d' },
+        };
+      },
+    }),
+  ],
 })
 export class CommonModule {}
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import type { AuthToken, AuthTokenPayload, AuthTokenPayloadType } from '@cm/shared/user';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  verifyToken(token: string): AuthTokenPayloadType {
+    return this.jwtService.verify<AuthTokenPayloadType>(token);
+  }
+
+  generateToken(payload: AuthTokenPayload): AuthToken {
+    return {
+      accessToken: this.jwtService.sign<AuthTokenPayloadType>(
+        { ...payload, tokenType: 'access' },
+        { expiresIn: '15m' },
+      ),
+      refreshToken: this.jwtService.sign<AuthTokenPayloadType>(
+        { ...payload, tokenType: 'refresh' },
+        {
+          expiresIn: '7d',
+        },
+      ),
+    };
+  }
+}
